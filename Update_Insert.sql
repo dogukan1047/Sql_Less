be9a6ee64e7212552bbe6328e082e22551f62a59
DogukanPolatel <70372233+dogukan1047@users.noreply.github.com>
2023-02-26 21:32:06 +0300

    Sql_Again

---
+++ Update_Insert.sql
@@ -0,0 +1,5 @@
+Insert into Student values('Metin','Kara',45678932115,'E',15)
+
+select * from Student
+
+Update Student set Cinsiyet='E' where Cinsiyet='e'
---
+++ Where_Select_.sql
@@ -0,0 +1,9 @@
+SELECT *FROM Student 
+
+SELECT * FROM Student WHERE Cinsiyet='E'
+
+Select Ad ,SoyAd from Student where Cinsiyet='K' order By SoyAd Desc
+
+select Ad from Student Where Soyad in ('Polat','Acar')
+
+Alter Table Student Add SinavNotu tinyint DEFAULT 0
